0feaf6c9084cd3ce354387d6e42b20411ee6b29d
Chris Pavlicek <chris.pavlicek@velocityapp.com>
2019-05-29 13:45:03 +0100

    fix: function

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,8 +2,4 @@ alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
 
-alias kubenode="kubectl get nodes --no-headers | awk '{print $1}' | xargs -I {} sh -c 'echo {}; kubectl describe node {} | grep Allocated -A 5 | grep -ve Event -ve Allocated -ve percent -ve -- ; echo"
-
-alias kubenode="kubectl get nodes --no-headers | awk '{print $1}' | xargs -I {} sh -c 'echo {}; kubectl describe node {} | grep Allocated -A 5 | grep -ve Event -ve Allocated -ve percent -ve -- ; echo"
-
 alias ku="kubectl"
